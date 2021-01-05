79cd2b880a57cfe45a7bf96180fb7b5bf0a3a237
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2021-01-05 23:11:29 +0200

    start

---
+++ src/main/java/com/oktenweb/springbootstpr/SpringBootStPrApplication.java
@@ -0,0 +1,13 @@
+package com.oktenweb.springbootstpr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStPrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStPrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oktenweb/springbootstpr/SpringBootStPrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oktenweb.springbootstpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStPrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
