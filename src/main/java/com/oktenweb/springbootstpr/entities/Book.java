f4a1206a0f2fcd244f0bf22fd6f3991c79428ba1
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2021-01-05 23:54:18 +0200

    start

---
+++ src/main/java/com/oktenweb/springbootstpr/entities/Book.java
@@ -0,0 +1,14 @@
+package com.oktenweb.springbootstpr.entities;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+
+public class Book {
+
+    private int id;
+    private String name;
+    private String author;
+}
