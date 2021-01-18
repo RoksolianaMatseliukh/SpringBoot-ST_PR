10834ccbe16641727d2a028851c6928a6d19e06a
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2021-01-18 16:16:28 +0200

    practice

--- src/main/java/com/oktenweb/springbootstpr/controllers/MovieController.java
+++ src/main/java/com/oktenweb/springbootstpr/controllers/MovieController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,10 +18,31 @@ public class MovieController {
     @Autowired
     private MovieRepository movieRepository;
 
-    @GetMapping
-    public List<Movie> getMovies() {
-        return movieRepository.findAll();
-    }
+//    @GetMapping
+//    public List<Movie> getMovies() {
+//        return movieRepository.findAll();
+//    }
+
+//    @GetMapping
+//    public List<Movie> getMoviesByQueries(@RequestParam int gt) {
+//        return movieRepository.findMoviesByDurationGreaterThan(gt);
+//    }
+
+//    @GetMapping
+//    public Movie getMoviesByQueries(@RequestParam int id, String name) {
+//        return movieRepository.findFirstByIdAndName(id, name);
+//    }
+
+//    @GetMapping
+//    public List<Movie> getMoviesByQueries(@RequestParam List<Integer> ids) {
+////        http://localhost:8080/movies?ids=1,3
+//        return movieRepository.findMoviesByIdIsIn(ids);
+//    }
+
+//    @GetMapping
+//    public List<Movie> getMoviesByQueries(@RequestParam int duration) {
+//        return movieRepository.findAllByDurationGreaterThan(duration);
+//    }
 
     @GetMapping("/{id}")
     public Optional<Movie> getMovieById(@PathVariable int id){
@@ -49,13 +71,20 @@ public class MovieController {
         return movieRepository.saveAndFlush(editedMovie);
     }
 
-    @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteMovieById(@PathVariable int id) {
-        if (!movieRepository.existsById(id)) {
-            throw new IllegalArgumentException("movie doesn't exists with " + id + " id");
-        }
+//    @DeleteMapping("/{id}")
+//    @ResponseStatus(HttpStatus.NO_CONTENT)
+//    public void deleteMovieById(@PathVariable int id) {
+//        if (!movieRepository.existsById(id)) {
+//            throw new IllegalArgumentException("movie doesn't exists with " + id + " id");
+//        }
+//
+//        movieRepository.deleteById(id);
+//    }
 
-        movieRepository.deleteById(id);
+    @DeleteMapping
+    @Transactional
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMovieById(@RequestParam int duration) {
+       movieRepository.removeByDurationGreaterThan(duration);
     }
 }
--- src/main/java/com/oktenweb/springbootstpr/dao/MovieRepository.java
+++ src/main/java/com/oktenweb/springbootstpr/dao/MovieRepository.java
@@ -3,5 +3,12 @@ package com.oktenweb.springbootstpr.dao;
 import com.oktenweb.springbootstpr.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    List<Movie> findMoviesByDurationGreaterThan(int gt);
+    Movie findFirstByIdAndName(int id, String name);
+    List<Movie> findMoviesByIdIsIn(List<Integer> ids);
+    List<Movie> findAllByDurationGreaterThan(int duration);
+    void removeByDurationGreaterThan(int duration);
 }
